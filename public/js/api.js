29d1ba97252e2162a09b68ca5d7df35c81f6d901
Ronaldo Alves <PurpleRain182@users.noreply.github.com>
2022-12-22 22:11:22 -0300

    Finalização
    Projeto finalizado, agora é hora de adicionar funcionalidades extras.

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "2fd9c0b8499b9da41eabc589b2da7db8"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
